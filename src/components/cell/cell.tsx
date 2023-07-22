60c7b051d9fb2d2225eca90847f1bc9376283ceb
cwtwelve <cwakefield12@gmail.com>
2023-07-22 11:10:11 -0500

    Diagnosing cell sizing issue

--- src/components/cell/cell.tsx
+++ src/components/cell/cell.tsx
@@ -6,14 +6,11 @@ const Cell = (props: { walls: [boolean, boolean, boolean, boolean] }) => {
 	const { walls } = props;
 	const [topBorder, rightBorder, bottomBorder, leftBorder] = walls;
 
-	const widthClass = `w-[${40}px]`;
-	const heightClass = `h-[${40}px]`;
-
 	return (
 		<div
 			className={classNames(
-				widthClass,
-				heightClass,
+				`w-[${40}px]`,
+				`h-[${40}px]`,
 				"hover:bg-orange-400",
 				{ "border-t": topBorder },
 				{ "border-r": rightBorder },
